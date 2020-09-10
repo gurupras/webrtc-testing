9b53beee887ee9d9976786946e11958ecbafad65
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-09-09 21:48:49 -0400

    remote: Added a socket.io server

--- src/remote/server.js
+++ src/remote/server.js
@@ -11,6 +11,11 @@ globalLoggers.push(new ConsoleLogger())
 const log = new Logger('remote:server')
 
 const server = http.createServer(app)
+
+const io = new IO(server, {
+  timeout: 120000
+})
+
 server.listen(config.remote.port, () => {
   log.info(`HTTP server listening on port ${config.remote.port}`)
 })
